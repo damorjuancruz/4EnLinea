ad7cba17603af217b52a2639f15391423b8b9714
Juan Cruz D'Amor <damorjuancruz@gmail.com>
2021-05-22 13:23:03 -0300

    Renombrado de las funciones para que todas usen snake_case

--- src/prototipo.py
+++ src/prototipo.py
@@ -2,7 +2,7 @@
 
 # Tablero: 6 filas x 7 columnas
 
-def dibujarTablero(tablero):
+def dibujar_tablero(tablero):
     print('+---------------------+')
     for fila in tablero:
         print('|', end='')
@@ -14,19 +14,19 @@ def dibujarTablero(tablero):
         print('|')
     print('+---------------------+')
 
-def completarTableroEnOrden(secuencia, tablero):
+def completar_tablero_en_orden(secuencia, tablero):
     for i, columna in enumerate(secuencia):
-        soltarFichaEnColumna((i % 2) + 1, columna, tablero)
+        soltar_ficha_en_columna((i % 2) + 1, columna, tablero)
     return tablero
 
-def soltarFichaEnColumna(ficha, columna, tablero):
+def soltar_ficha_en_columna(ficha, columna, tablero):
     for fila in range(6, 0, -1):
         if tablero[fila - 1][columna - 1] == 0:
             tablero[fila - 1][columna - 1] = ficha
             break
 
 # Devuelve un tablero vacio
-def tableroVacio():
+def tablero_vacio():
     return [[0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0],
@@ -53,7 +53,7 @@ def columnas(tablero):
 def filas(tablero):
     return tablero
 
-def validarSecuencia(secuencia):
+def validar_secuencia(secuencia):
     for columna in secuencia:
         if columna < 1 or columna > 7:
             return False
@@ -62,8 +62,8 @@ def validarSecuencia(secuencia):
 secuencia = [1, 2, 3, 4, 5, 3, 6, 7]
 tablero = []
 
-if validarSecuencia(secuencia):
-    tablero = completarTableroEnOrden(secuencia, tableroVacio())
-    dibujarTablero(tablero)
+if validar_secuencia(secuencia):
+    tablero = completar_tablero_en_orden(secuencia, tablero_vacio())
+    dibujar_tablero(tablero)
 else:
     print("Error: las columnas deben estar entre el 1 y el 7.")
